cd419b1aec1f5d4553dd65e323c27fe1f50d0eaf
nils <nils.haentjens@maine.edu>
2016-03-14 16:51:54 -0400

    Update comments of estimate_cphyto

--- estimate_cphyto.m
+++ estimate_cphyto.m
@@ -1,18 +1,20 @@
 function c_phyto = estimate_cphyto(bbp, lambda, method)
-%ESTIMATE_CPHYTO Phytoplankton carbon biomass Cphyto is estimated from bbp(440)
-%   Two empirical relationship are available:
+%ESTIMATE_CPHYTO PEstimate phytoplankton carbon biomass (Cphyto) from
+%   particulate backscattering (bbp). The two methods available are based on
+%   relation with bbp at 440 nm, the parameter lambda can be used to
+%   shift the relation to 700 nm.
+%
+%References:
 %     Michael J. Behrenfeld, Emmanuel Boss, David a. Siegel, and Donald M. Shea.
-%   Carbon-based ocean produc- tivity and phytoplankton physiology from space. 
+%   Carbon-based ocean produc- tivity and phytoplankton physiology from space.
 %   Global Biogeochemical Cycles, 19(1):1?14, 2005. ISSN 08866236. doi: 10.1029/2004GB002299.
 %     Jason R. Graff, Toby K. Westberry, Allen J. Milligan, Matthew B. Brown,
-%   Giorgio Dall?Olmo, Virginie van Dongen-Vogels, Kristen M. Reifel, and
+%   Giorgio Dall'Olmo, Virginie van Dongen-Vogels, Kristen M. Reifel, and
 %   Michael J. Behrenfeld. Analytical phytoplankton carbon measurements spanning
-%   diverse ecosystems. Deep-Sea Research Part I: Oceanographic Research Papers, 
-%   102:16?25, 2015. ISSN 09670637. doi: 10.1016/j.dsr.2015.04.006. 
+%   diverse ecosystems. Deep-Sea Research Part I: Oceanographic Research Papers,
+%   102:16?25, 2015. ISSN 09670637. doi: 10.1016/j.dsr.2015.04.006.
 %   URL http://dx.doi.org/10.1016/ j.dsr.2015.04.006.
-% 
-%   If require to change wavelenght of bbp the following method is used:
-%   Emmanuel Boss, Marc Picheral, Thomas Leeuw, Alison Chase, Eric Karsenti,
+%     Emmanuel Boss, Marc Picheral, Thomas Leeuw, Alison Chase, Eric Karsenti,
 %   Gabriel Gorsky, Lisa Taylor, Wayne Slade, Josephine Ras, and Herve Claustre.
 %   The characteristics of particulate absorption, scattering and attenuation
 %   coefficients in the surface ocean; Contribution of the Tara Oceans expedition.
@@ -22,19 +24,18 @@ function c_phyto = estimate_cphyto(bbp, lambda, method)
 %
 %Syntax:  [ c_phyto ] = estimate_cphyto( bbp, lambda, method )
 %
-%Inputs: 
+%Inputs:
 %    Required:
-%        bbp NxM double corresponding to the values of the VSF at one angle in m^{-1}
+%        bbp NxM double, particulate backscattering (m^{-1})
 %    Optional:
-%        lambda 1x1 or 1xM double corresponding to the wavelength in nm
+%        lambda 1x1 or 1xM double, wavelength (nm)
 %           default: 700
-%        method string of the name of the method to use
-%           default: 'Graff2015'
-%           Behrenfeld2005: c_phyto = 13000 ? (bbp(440) ? 0.00035)
-%           Graff2015: c_phyto = 12128 ? bbp(440) + 0.59
+%        method string, method to use for the estimation
+%           Graff2015: based on empirical data (default)
+%           Behrenfeld2005: based on a model
 %
 %Outputs:
-%   bbp NxM double corresponding to c_phyto in mg.m^{?3}
+%   Cphyto NxM double phytoplankton carbon (mg.m^{-3})
 %
 %Examples:
 % [poc] = estimate_cphyto(bbp);
@@ -45,7 +46,7 @@ function c_phyto = estimate_cphyto(bbp, lambda, method)
 % Author: Nils Haentjens, Ms, University of Maine
 % Email: nils.haentjens@maine.edu
 % Created: February 5th 2016
-% Last update: February 5th 2016   
+% Last update: February 5th 2016
 
 % Check Nargin
 if nargin > 3
@@ -69,17 +70,17 @@ end;
 % Resize lambda
 lambda = bsxfun(@times, ones(size(bbp)), lambda);
 
-% Estimate poc
+% Estimate c_phyto
 switch method
   case 'Behrenfeld2005'
     % switch to bbp(700)
     bbp_440 = bbp .* (440 ./ lambda) .^ (-0.78);
-    % estimate poc from bbp(700)
+    % estimate c_phyto from bbp(700)
     c_phyto = 13000 * (bbp_440 - 0.00035);
   case 'Graff2015'
     % switch to bbp(700)
     bbp_440 = bbp .* (440 ./ lambda) .^ (-0.78);
-    % estimate poc from bbp(700)
+    % estimate c_phyto from bbp(700)
     c_phyto = 12128 * bbp_440 + 0.59;
   otherwise
     error('Unknown method %s', method);
